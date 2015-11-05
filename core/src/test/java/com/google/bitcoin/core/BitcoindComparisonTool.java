71829acce2f182d415b2b29c0372709608a9335c
upgradeadvice <upgradeadvice@gmail.com>
2015-11-04 23:04:23 -0600

    Rename monetaryunitj

--- core/src/test/java/com/google/bitcoin/core/BitcoindComparisonTool.java
+++ core/src/test/java/com/google/bitcoin/core/BitcoindComparisonTool.java
@@ -33,8 +33,8 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * A tool for comparing the blocks which are accepted/rejected by bitcoind/quarkcoinj
- * It is designed to run as a testnet-in-a-box network between a single bitcoind node and quarkcoinj
+ * A tool for comparing the blocks which are accepted/rejected by bitcoind/monetaryunitj
+ * It is designed to run as a testnet-in-a-box network between a single bitcoind node and monetaryunitj
  * It is not an automated unit-test because it requires a bit more set-up...read comments below
  */
 public class BitcoindComparisonTool {
@@ -206,7 +206,7 @@ public class BitcoindComparisonTool {
                 bitcoind.ping().get();
                 if (!chain.getChainHead().getHeader().getHash().equals(bitcoindChainHead)) {
                     differingBlocks++;
-                    log.error("bitcoind and quarkcoinj acceptance differs on block \"" + block.ruleName + "\"");
+                    log.error("bitcoind and monetaryunitj acceptance differs on block \"" + block.ruleName + "\"");
                 }
                 log.info("Block \"" + block.ruleName + "\" completed processing");
             } else if (rule instanceof MemoryPoolState) {
@@ -243,7 +243,7 @@ public class BitcoindComparisonTool {
         }
 
         log.info("Done testing.\n" +
-                "Blocks which were not handled the same between bitcoind/quarkcoinj: " + differingBlocks + "\n" +
+                "Blocks which were not handled the same between bitcoind/monetaryunitj: " + differingBlocks + "\n" +
                 "Blocks which should/should not have been accepted but weren't/were: " + invalidBlocks + "\n" +
                 "Transactions which were/weren't in memory pool but shouldn't/should have been: " + mempoolRulesFailed + "\n" +
                 "Unexpected inv messages: " + unexpectedInvs.get());
